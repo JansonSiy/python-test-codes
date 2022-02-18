6d40a72cf274bd4d7503a7d7a07efca1878dbeab
jhrs <66736876+JansonSiy@users.noreply.github.com>
2022-02-19 00:22:34 +0800

    fixed comments

--- data_structures.py
+++ data_structures.py
@@ -1,82 +1,83 @@
-#DICTIONARY
-#note: a key can be a string or an integer
+# DICTIONARY
+
+# note: a key can be a string or an integer
 dictionary_1 = {"user_id":123, "name":"janson", "location":"(1.11, -11.111)"}
 print(dictionary_1)
 
-#adding a dictionary
+# adding a dictionary
 dictionary_1["position"] = "web developer"
 print("adding a key-value pair = ", dictionary_1)
 
 dictionary_2 = {"user_id":456, "name":"jefferson", "location":"(2.22, -22.222)"}
 print(dictionary_2["user_id"])
 
-#catching an error in a dictionary
+# catching an error in a dictionary
 try:
     print(dictionary_2["position"])
 except KeyError:
     print("error: not found")
 
-#another way of printing a dictionary
+# another way of printing a dictionary
 print(dict([("jan", 123), ("jeff", 456), ("jet", 789)]))
-#or
+# or
 print(dict(jan=123, jeff=456, jet=789))
 
-#LIST
+# LIST
 fruits = ["apple", "grapes", "mango"]
 print("example = ", fruits)
 
-#.insert()
+# .insert()
 fruits.insert(2, "banana")
 print("this is insert = ", fruits)
 
-#.append()
+# .append()
 fruits.append("melon")
 print("this is append = ", fruits)
 
-#.pop()
+# .pop()
 print("this is pop = ", fruits.pop())
 print("this is pop = ", fruits)
 
-#.reverse()
+# .reverse()
 fruits.reverse()
 print("this is reverse = ", fruits)
 
-#.count()
+# .count()
 print("this is count of apple = ", fruits.count("apple"))
 
-#.copy()
+# .copy()
 fruits_2 = fruits.copy()
 print("this is fruits_2 from copy = ", fruits_2)
 
-#.extend()
+# .extend()
 fruits_3 = ["orange", "watermelon", "coconut"]
 fruits.extend(fruits_3)
 print("this is extend = ", fruits)
-#or
-#print("this is extend = ", fruits + fruits_3)
+# or
+# print("this is extend = ", fruits + fruits_3)
 
-#.sort()
+# .sort()
 fruits.sort()
 print("this is sort = ", fruits)
 
-#.remove()
+# .remove()
 fruits.remove("apple")
 print("this is remove = ", fruits)
 
-#.clear()
+# .clear()
 fruits.clear()
 print("this is clear = ", fruits)
 
-#.index()
+# .index()
 janson = ['siy', 'Pasig', 'siy', 27]
 
-#note: will stop upon first occurence
+# note: will stop upon first occurence
 print("this is simple indexing = ", janson.index('siy'))
 
-#note: ignored 'siy' under index 0 because you told it to start at index 1
+# note: ignored 'siy' under index 0 because you told it to start at index 1
 print("this is targeted indexing = ", janson.index('siy', 1))
 
 jeff = [1, 2, 1, 1, 3, 4, 1]
 
-#note notes: started to look for 1 from index 4 to 7 then returned the index number of the first occurence
+# note notes: started to look for 1 from index 4 to 7 then returned the index number of the first occurence
 print("this is range indexing = ", jeff.index(1, 4, 7))
