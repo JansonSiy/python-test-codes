762998883ed505a0ce01729c99f718783ca8cf7c
jhrs <66736876+JansonSiy@users.noreply.github.com>
2022-02-22 16:26:44 +0800

    fixed whitespacing

--- csv.py
+++ csv.py
@@ -4,18 +4,18 @@ import csv
 def exportProducts(request):
     # since your are dealing with a CSV file, your content_type should be text/csv
     response = HttpResponse(content_type='text/csv')
-
-	# then you need to create a CSV writer
+    
+    # then you need to create a CSV writer
     writer = csv.writer(response)
     # after which create the header of the CSV file
     writer.writerow(['Name', 'Price', 'Quantity', 'Image URL', 'Description'])
-	
-	# for this part, looping was a better thing to do
-	# what you are doing is getting all the values of the keys declared in the header by using .values_list then, writing a row for each set of product values
+    
+    # for this part, looping was a better thing to do
+    # what you are doing is getting all the values of the keys declared in the header by using .values_list then, writing a row for each set of product values
     for product in Product.objects.all().values_list('name', 'price', 'quantity', 'image', 'description'):
         writer.writerow(product)
 
-	# for the last part, this tells the browser what to do with the response, in this case treat it as an attachment
+    # for the last part, this tells the browser what to do with the response, in this case treat it as an attachment
     response['Content-Disposition'] = 'attachment; filename="products.csv"'
     
     return response
