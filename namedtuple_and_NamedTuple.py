2edaa2ec352247e469566fd70a2865e20e75fb4b
jhrs <66736876+JansonSiy@users.noreply.github.com>
2022-02-22 16:32:49 +0800

    added sample codes for namedtuple and NamedTuple

---
+++ namedtuple_and_NamedTuple.py
@@ -0,0 +1,28 @@
+from collections import namedtuple
+
+Color = namedtuple('Color', ['red', 'green', 'blue'])
+
+myColor = Color(55, 155, 255)
+
+print(myColor[0])
+# output 55
+
+print(myColor.blue)
+# output 255
+
+
+
+from typing import NamedTuple
+
+class Person(NamedTuple):
+    first_name: str
+		last_name: str
+    age: int
+
+# equivalent to
+
+# Person = collections.namedtuple('Person', ['name', 'age'])
+
+personOne = Person('Jan', 'Siy', 27)
+
+print(personOne)
