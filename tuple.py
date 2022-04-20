752428cd218efacf3fa0298d812821ae87f72706
jhrs <66736876+JansonSiy@users.noreply.github.com>
2022-04-20 14:32:42 +0800

    tuple sample codes

---
+++ tuple.py
@@ -0,0 +1,29 @@
+numbers = (1, 2, 3)
+type(numbers)
+# <class 'tuple'>
+
+
+# Tuple Packing
+another_numbers = 1, 2, 3
+another_numbers
+# (1, 2, 3)
+type(another_numbers)
+# <class 'tuple'>
+
+
+# Unpacking A Tuple
+one, two, three = (1, 2, 3)
+one
+# 1
+two
+# 2
+three
+# 3
+
+one, two, three = 1, 2, 3
+one
+# 1
+two
+# 2
+three
+# 3
