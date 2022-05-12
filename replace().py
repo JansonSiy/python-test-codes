dabff5021e80792116bba51c5edf52b53b744b1f
jhrs <66736876+JansonSiy@users.noreply.github.com>
2022-05-12 16:03:46 +0800

    replace test code

---
+++ replace().py
@@ -0,0 +1,7 @@
+string = "janson, please finish all your tasks, let me know if you have any questions."
+# string
+# 'janson, please finish all your tasks, let me know if you have any questions.'
+
+replace = string.replace(",", ";")
+# replace
+# 'janson; please finish all your tasks; let me know if you have any questions.'
