80c90eba8d5d329e4aabf133eea189b253125df7
jhrs <66736876+JansonSiy@users.noreply.github.com>
2023-04-12 20:54:51 +0800

    updated comments

--- iterator(chunk_size=1000).py
+++ iterator(chunk_size=1000).py
@@ -1,7 +1,7 @@
 # let's say users has hundred thousands of objects
 users = User.objects.all()
 
-# iterator(chunk_size=1000) will only store 1000 objects in memory/cache at a time to help with the heavy querying
+# iterator(chunk_size=1000) will only store 1000 objects in memory/cache at a time to help with the heavy querying/decreasing memory consumption
 for user in users.iterator(chunk_size=1000):
     user.full_name = f'{user.first_name} {user.last_name}'
     user.save()
